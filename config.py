012ee1855380ecb9c8f9b4678eeeb61d281b2201
Omolola Oladeinde <aderonke11@gmail.com>
2022-06-08 22:03:19 +0100

    connect to database

--- config.py
+++ config.py
@@ -7,7 +7,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:oladeinde@localhost:5432/fyyur2'
 
-
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
